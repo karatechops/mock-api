2bd9104729044a694bf9864f8870b9ecf21082d2
karatehcops <contact@lazers.tv>
2019-06-14 16:11:48 -0400

    Add mock endpoints.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import users from './users';
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/api/bootstrap', (req, res) => {
-  res.status(200).send({ gremlins: ['Stack', 'Alan', 'Eric', 'Chris', 'Shimi', 'Alex', 'Ian', 'Brittany', 'Orestis'] });
+app.get('/api/servers', (req, res) => {
+  res.status(200).send({ server: [
+    { name: 'Barney', value: 50, color: 'light-3' },
+    { name: 'Fred', value: 30, color: 'neutral-1' },
+    { name: 'Wilma', value: 20, color: 'brand' },
+    { name: 'Betty', value: 10, color: 'light-3' },
+    { name: 'Bamm-Bamm', value: 5, color: 'neutral-1' },
+  ] });
+});
+
+app.get('/api/users', (req, res) => {
+  res.status(200).send({ users });
 });
 
 app.listen(3001, () => {
---
+++ src/users.js
@@ -0,0 +1,532 @@
+export default [
+  {
+    status: "online",
+    gender: "male",
+    name: {
+      title: "mr",
+      first: "adham",
+      last: "hermes"
+    },
+    location: {
+      street: "5217 damstraat",
+      city: "hulst",
+      state: "zuid-holland",
+      postcode: 23528,
+      coordinates: {
+        latitude: "44.4442",
+        longitude: "-33.9352"
+      },
+      timezone: {
+        offset: "-7:00",
+        description: "Mountain Time (US & Canada)"
+      }
+    },
+    email: "adham.hermes@example.com",
+    login: {
+      uuid: "042826f2-a3e6-4744-947a-d679e3db9733",
+      username: "heavymouse836",
+      password: "damian",
+      salt: "Oo4ACss3",
+      md5: "c9d4d339de2fca4d35695a5a57534de5",
+      sha1: "26de8399fe336ae831d5530f1307551caeff1180",
+      sha256: "577331e2b1acaa3ca189feca6d47f4a08ae226798f73d79442beb4658373651a"
+    },
+    dob: {
+      date: "1967-01-01T18:20:47Z",
+      age: 52
+    },
+    registered: {
+      date: "2010-02-26T01:38:38Z",
+      age: 9
+    },
+    phone: "(828)-754-2246",
+    cell: "(817)-399-9696",
+    id: {
+      name: "BSN",
+      value: "71381039"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/61.jpg",
+      medium: "https://randomuser.me/api/portraits/med/men/61.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/61.jpg"
+    },
+    nat: "NL"
+  },
+  {
+    status: "idle",
+    gender: "male",
+    name: {
+      title: "mr",
+      first: "duane",
+      last: "henderson"
+    },
+    location: {
+      street: "1466 prospect rd",
+      city: "kalgoorlie",
+      state: "northern territory",
+      postcode: 6835,
+      coordinates: {
+        latitude: "-16.8732",
+        longitude: "54.7949"
+      },
+      timezone: {
+        offset: "+10:00",
+        description: "Eastern Australia, Guam, Vladivostok"
+      }
+    },
+    email: "duane.henderson@example.com",
+    login: {
+      uuid: "d2c5fef9-eab2-45d3-86e0-446f3a741326",
+      username: "orangewolf379",
+      password: "waffle",
+      salt: "mkyUTdcu",
+      md5: "b5144804b00b6ec6ea1761352a149007",
+      sha1: "c38b83b8f866962fc2b56d0fc6ce4a6d7743cfd7",
+      sha256: "592a14e7953be6c98e43706916e4cdfc6f8099170a42b10d1f43c910d377086f"
+    },
+    dob: {
+      date: "1986-10-08T03:40:28Z",
+      age: 32
+    },
+    registered: {
+      date: "2005-05-06T16:02:17Z",
+      age: 14
+    },
+    phone: "05-9424-3831",
+    cell: "0413-876-900",
+    id: {
+      name: "TFN",
+      value: "568966385"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/35.jpg",
+      medium: "https://randomuser.me/api/portraits/med/men/35.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/35.jpg"
+    },
+    nat: "AU"
+  },
+  {
+    status: "online",
+    gender: "male",
+    name: {
+      title: "monsieur",
+      first: "grégory",
+      last: "thomas"
+    },
+    location: {
+      street: "2646 avenue joliot curie",
+      city: "obergerlafingen",
+      state: "thurgau",
+      postcode: 8939,
+      coordinates: {
+        latitude: "43.7076",
+        longitude: "86.3152"
+      },
+      timezone: {
+        offset: "-10:00",
+        description: "Hawaii"
+      }
+    },
+    email: "grégory.thomas@example.com",
+    login: {
+      uuid: "005f8f4c-358f-4746-9d53-26dbae0089de",
+      username: "ticklishgoose339",
+      password: "blackout",
+      salt: "sqDclBEO",
+      md5: "2b5d60942bb4ba4ef40af84954b4654e",
+      sha1: "95013d190e17bdd13cf8bdce85107a89507f36c6",
+      sha256: "d168fb94cc2799e8cb85b6bbb8ea5085f3725d10b3e17e79c9707038a374cbef"
+    },
+    dob: {
+      date: "1995-03-15T07:28:37Z",
+      age: 24
+    },
+    registered: {
+      date: "2003-01-25T01:20:47Z",
+      age: 16
+    },
+    phone: "(763)-436-9308",
+    cell: "(674)-161-5793",
+    id: {
+      name: "AVS",
+      value: "756.0400.3152.78"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/53.jpg",
+      medium: "https://randomuser.me/api/portraits/med/men/53.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/53.jpg"
+    },
+    nat: "CH"
+  },
+  {
+    status: "online",
+    gender: "male",
+    name: {
+      title: "mr",
+      first: "obed",
+      last: "planting"
+    },
+    location: {
+      street: "1690 keulsekade",
+      city: "nijmegen",
+      state: "drenthe",
+      postcode: 42795,
+      coordinates: {
+        latitude: "29.6774",
+        longitude: "69.6564"
+      },
+      timezone: {
+        offset: "+4:00",
+        description: "Abu Dhabi, Muscat, Baku, Tbilisi"
+      }
+    },
+    email: "obed.planting@example.com",
+    login: {
+      uuid: "4d9a2376-f141-4ac7-8470-377a2d54f08e",
+      username: "smallcat533",
+      password: "clipper",
+      salt: "PKcDHWJ0",
+      md5: "c144156425a859a812e17757bd41994c",
+      sha1: "f9e46d24df261d576a4a67199250408341362210",
+      sha256: "8d46488544f9be74a7d4b747c3889bd0bcbf5cab3c9452eaef417a0cfef6eed9"
+    },
+    dob: {
+      date: "1976-03-27T05:41:44Z",
+      age: 43
+    },
+    registered: {
+      date: "2005-02-09T19:33:06Z",
+      age: 14
+    },
+    phone: "(612)-509-6333",
+    cell: "(987)-828-1316",
+    id: {
+      name: "BSN",
+      value: "33651083"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/9.jpg",
+      medium: "https://randomuser.me/api/portraits/med/men/9.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/9.jpg"
+    },
+    nat: "NL"
+  },
+  {
+    status: "offline",
+    gender: "male",
+    name: {
+      title: "mr",
+      first: "hector",
+      last: "richardson"
+    },
+    location: {
+      street: "4558 station road",
+      city: "new ross",
+      state: "sligo",
+      postcode: 49496,
+      coordinates: {
+        latitude: "39.4484",
+        longitude: "67.3757"
+      },
+      timezone: {
+        offset: "+9:30",
+        description: "Adelaide, Darwin"
+      }
+    },
+    email: "hector.richardson@example.com",
+    login: {
+      uuid: "5669f51e-ab62-4a28-8e50-7005183bdb2a",
+      username: "ticklishzebra318",
+      password: "555666",
+      salt: "StXhxIxQ",
+      md5: "16be1a6bf0ee99f4adb260ba2dd6fcaf",
+      sha1: "eebf8097637c97f0f6a2ed450b57e841fea89576",
+      sha256: "731c22bed6018d30889b6e314cb1b562c6c5261f011403593a167aae0817989d"
+    },
+    dob: {
+      date: "1981-08-03T12:23:40Z",
+      age: 37
+    },
+    registered: {
+      date: "2015-09-12T17:57:09Z",
+      age: 3
+    },
+    phone: "051-068-2223",
+    cell: "081-852-6357",
+    id: {
+      name: "PPS",
+      value: "8718088T"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/33.jpg",
+      medium: "https://randomuser.me/api/portraits/med/men/33.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/33.jpg"
+    },
+    nat: "IE"
+  },
+  {
+    status: "idle",
+    gender: "male",
+    name: {
+      title: "mr",
+      first: "patrick",
+      last: "wang"
+    },
+    location: {
+      street: "2205 spey street",
+      city: "palmerston north",
+      state: "hawke's bay",
+      postcode: 44051,
+      coordinates: {
+        latitude: "8.8970",
+        longitude: "42.0529"
+      },
+      timezone: {
+        offset: "-7:00",
+        description: "Mountain Time (US & Canada)"
+      }
+    },
+    email: "patrick.wang@example.com",
+    login: {
+      uuid: "27915a90-78c9-4f3c-a792-1a1380fa554f",
+      username: "purplemouse664",
+      password: "reserve",
+      salt: "yjNVYKFM",
+      md5: "8d75e67ff52bc42313c7697e65a57c0f",
+      sha1: "15250ea7b260d99ac5dd4c93a069f662018f49e9",
+      sha256: "6de6236aff4ef172aed4346a3b3aeb96a010654c607d98ad472da9c72a52fe06"
+    },
+    dob: {
+      date: "1971-06-01T14:58:03Z",
+      age: 48
+    },
+    registered: {
+      date: "2018-05-13T10:34:52Z",
+      age: 1
+    },
+    phone: "(023)-406-5578",
+    cell: "(912)-835-0562",
+    id: {
+      name: "",
+      value: null
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/71.jpg",
+      medium: "https://randomuser.me/api/portraits/med/men/71.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/71.jpg"
+    },
+    nat: "NZ"
+  },
+  {
+    status: "offline",
+    gender: "female",
+    name: {
+      title: "mrs",
+      first: "jasmine",
+      last: "williams"
+    },
+    location: {
+      street: "1341 simcoe st",
+      city: "sandy lake",
+      state: "northwest territories",
+      postcode: "K1Q 1S9",
+      coordinates: {
+        latitude: "71.8846",
+        longitude: "91.0817"
+      },
+      timezone: {
+        offset: "+2:00",
+        description: "Kaliningrad, South Africa"
+      }
+    },
+    email: "jasmine.williams@example.com",
+    login: {
+      uuid: "d4d153a6-5a83-40d6-956c-e7cbc7b4c168",
+      username: "happywolf331",
+      password: "bond",
+      salt: "HC1vbB2Y",
+      md5: "9310c7377e6d9057a91ff5f0ad4792be",
+      sha1: "de5e5c946e56ab4767b5e5eb87eed9a099b04ca6",
+      sha256: "3d22ac40bae006fa00bb1a956ed1a942250c42a6b56d835861c126fa804ee043"
+    },
+    dob: {
+      date: "1992-08-17T18:55:40Z",
+      age: 26
+    },
+    registered: {
+      date: "2013-01-15T00:50:21Z",
+      age: 6
+    },
+    phone: "046-242-3964",
+    cell: "181-321-8209",
+    id: {
+      name: "",
+      value: null
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/17.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/17.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/17.jpg"
+    },
+    nat: "CA"
+  },
+  {
+    status: "online",
+    gender: "female",
+    name: {
+      title: "mademoiselle",
+      first: "lucia",
+      last: "roger"
+    },
+    location: {
+      street: "6718 rue de la charité",
+      city: "bedretto",
+      state: "appenzell ausserrhoden",
+      postcode: 6938,
+      coordinates: {
+        latitude: "-21.8837",
+        longitude: "67.7921"
+      },
+      timezone: {
+        offset: "-5:00",
+        description: "Eastern Time (US & Canada), Bogota, Lima"
+      }
+    },
+    email: "lucia.roger@example.com",
+    login: {
+      uuid: "0df3bd59-1ae3-4335-994a-58ccb2d1ddd0",
+      username: "silverfish540",
+      password: "mark",
+      salt: "aeQhXtuK",
+      md5: "a3582cea0ad5c935a53a2c846184e13d",
+      sha1: "17588c6f15c34f0836ffb7e6acbdfdfd83531afe",
+      sha256: "159069c9845226414d5adf50749cb34280c4dd4b985423204caffba1cbda9cbd"
+    },
+    dob: {
+      date: "1966-02-10T01:07:54Z",
+      age: 53
+    },
+    registered: {
+      date: "2004-02-25T23:44:16Z",
+      age: 15
+    },
+    phone: "(612)-012-6158",
+    cell: "(797)-572-5676",
+    id: {
+      name: "AVS",
+      value: "756.9326.5105.38"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/80.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/80.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/80.jpg"
+    },
+    nat: "CH"
+  },
+  {
+    status: "online",
+    gender: "female",
+    name: {
+      title: "mrs",
+      first: "eden",
+      last: "walker"
+    },
+    location: {
+      street: "6886 great north road",
+      city: "upper hutt",
+      state: "hawke's bay",
+      postcode: 39986,
+      coordinates: {
+        latitude: "57.7507",
+        longitude: "124.7703"
+      },
+      timezone: {
+        offset: "+5:45",
+        description: "Kathmandu"
+      }
+    },
+    email: "eden.walker@example.com",
+    login: {
+      uuid: "33c94662-acd9-489b-99a8-fb04560a99bd",
+      username: "smallkoala273",
+      password: "welcome1",
+      salt: "IuK2aPoo",
+      md5: "f0b4acc5e232827ac605c56005cf016c",
+      sha1: "577d0c170332b816586e06ab2852cc78f4c43031",
+      sha256: "7173f9fbcd06bdde1a0450561d2b08611d0f314930227cddb8e6476567589470"
+    },
+    dob: {
+      date: "1984-09-16T01:24:31Z",
+      age: 34
+    },
+    registered: {
+      date: "2017-08-28T17:13:39Z",
+      age: 1
+    },
+    phone: "(062)-986-0933",
+    cell: "(243)-007-2645",
+    id: {
+      name: "",
+      value: null
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/58.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/58.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/58.jpg"
+    },
+    nat: "NZ"
+  },
+  {
+    status: "online",
+    gender: "female",
+    name: {
+      title: "miss",
+      first: "susana",
+      last: "torres"
+    },
+    location: {
+      street: "2302 ronda de toledo",
+      city: "torrejón de ardoz",
+      state: "navarra",
+      postcode: 65597,
+      coordinates: {
+        latitude: "77.8069",
+        longitude: "-127.2119"
+      },
+      timezone: {
+        offset: "-10:00",
+        description: "Hawaii"
+      }
+    },
+    email: "susana.torres@example.com",
+    login: {
+      uuid: "3d6808a3-1c9f-4882-9b60-87d507dd26f4",
+      username: "sadbutterfly982",
+      password: "bella",
+      salt: "OmjS5f2a",
+      md5: "4da84e7d7a488c6908f9822d5118b327",
+      sha1: "07091827657ad15c4dd9036a41124017a2bfb389",
+      sha256: "0c6328d38f6810d47434710ce92ec1e18e9f291912b3e5bd7f71777c4a204250"
+    },
+    dob: {
+      date: "1948-01-01T23:29:39Z",
+      age: 71
+    },
+    registered: {
+      date: "2013-02-05T21:53:41Z",
+      age: 6
+    },
+    phone: "913-401-759",
+    cell: "639-838-708",
+    id: {
+      name: "DNI",
+      value: "39861158-B"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/32.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/32.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/32.jpg"
+    },
+    nat: "ES"
+  }
+];
